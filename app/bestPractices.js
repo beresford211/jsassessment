af1e6cf833e63e743b37d61f2a6c1b60c33c08a7
steele2345 <beresford211@users.noreply.github.com>
2016-03-03 20:24:04 -0800

    [add]: add files for js-assessment

---
+++ app/bestPractices.js
@@ -0,0 +1,37 @@
+exports = (typeof window === 'undefined') ? global : window;
+
+/**
+ * This file defines an object with some methods. Some of these methods are
+ * populated incorrectly; your job is to fix them. Other methods are not
+ * populated at all; your job is to fill them out.
+ */
+
+exports.bestPracticesAnswers = {
+  globals : function() {
+  var myObject = {
+       name : 'Jory'
+    };
+
+    return myObject;
+  },
+
+  functions : function(flag) {
+    var first;
+    function getValue(x) { return x; }
+    if (flag) {
+      first = 'a';
+    } else {
+      first = 'b';
+    }
+
+    return getValue(first);
+  },
+
+  parseInt : function(num) {
+    return parseInt(num);
+  },
+
+  identity : function(val1, val2) {
+    return val1 === val2;
+  }
+};
