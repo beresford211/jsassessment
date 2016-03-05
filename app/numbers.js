44b6cec2639d6907151bdfaf363b3ff6e364e2fa
steele2345 <beresford211@users.noreply.github.com>
2016-03-04 19:30:17 -0800

    [update]: passed test for binary conversion

--- app/numbers.js
+++ app/numbers.js
@@ -9,11 +9,14 @@ exports.numbersAnswers = {
 
   },
 
-  convertToBinary: function(num) {
-
+  convertToBinary: function(n) {
+      if (n < 0 || n > 255 || n % 1 !== 0) {
+        throw new Error(n + " does not fit in a byte");
+      }
+      return ("000000000" + n.toString(2)).substr(-8);
   },
 
   multiply: function(a, b) {
-
+    return a * b;
   }
 };
