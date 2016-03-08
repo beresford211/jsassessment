fc01d1c7019340c5c563d3ec75d7ad4b0a6cabed
steele2345 <beresford211@users.noreply.github.com>
2016-03-07 22:42:29 -0800

    [update]: completed isUSD

--- app/strings.js
+++ app/strings.js
@@ -2,8 +2,24 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
+    console.log(str, amount);
+    var finalStr = "";
 
+    var finalObj = str.split("").reduce(function(prev, current){
+        if(prev[current] === undefined){
+          prev[current] = 1;
+        } else if (prev[current] < amount){
+          prev[current]++;
+          if(prev[current] === amount){
+            
+          }
+        }
+          return prev;
+    }, {});
+    console.log(finalStr);
+    // return Object.keys(finalObj).join("");
   },
+  
   wordWrap: function(str, cols) {
 
   },
